53f1aa0bdd5f711c6452b050ea3c0733bff67efc
Sree952 <sreepadhmapriya52@gmail.com>
2026-01-31 13:39:52 +0530

    Add CartEmptyException class for empty cart handling

---
+++ CartEmptyException.java
@@ -0,0 +1,10 @@
+package com.wipro.eshop.exception; 
+ 
+public class CartEmptyException extends Exception { 
+     
+ private static final long serialVersionUID = 1L; 
+ 
+ public CartEmptyException(String message) { 
+        super(message); 
+ }
+}
